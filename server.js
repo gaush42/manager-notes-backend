a7008f472634e6fb7595e48d7501c645fd4527f1
Gulam Gaush <mohammadgulamgaush@gmail.com>
2022-09-28 11:01:14 +0530

    created model, made connection to database, error log for database

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
 const express = require('express')
 const app = express();
 const path = require('path')
-const { logger } = require('./middleware/logger')
+const { logger, logEvents } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
+const connectDB = require('./config/dbConnect')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 4000
+
+connectDB()
 
 app.use(logger)
 app.use(cors(corsOptions))
@@ -30,6 +35,14 @@ app.all('*', (req, res)=>{
 
 app.use(errorHandler)
 
-app.listen(PORT, ()=> {
-    console.log(`Server is running on PORT ${PORT}`)
+mongoose.connection.once('open', () => {
+    console.log('Database Connected')
+    app.listen(PORT, ()=> {
+        console.log(`Server is running on PORT ${PORT}`)
+    })
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.codeName}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
 })
